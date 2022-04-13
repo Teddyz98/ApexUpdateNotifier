138662e84a2b897ef8e7b72493750c6b14c3fa76
danielepelleg <daniele.pellegrini@studenti.unipr.it>
2022-04-14 01:18:34 +0200

    Update main.py
    Add handlers on 'start' and 'map' commands

--- bot/main.py
+++ bot/main.py
@@ -1,28 +1,20 @@
 import dotenv
 import telebot
 import os
-from Utils import RESTreq as req
+from utils import get_battle_royale_current_map, get_map_informations
 
 dotenv.load_dotenv()
 API_KEY_TELEGRAM = os.environ.get('API_KEY_TELEGRAM')
-
 bot = telebot.TeleBot(API_KEY_TELEGRAM)
 
-print(req.getMapRotation())
+@bot.message_handler(commands=['start'])
+def greet(message):
+    bot.send_message(message.chat.id, """
+        Hi there 👋🏼, I am Apex Update Notifier Bot. I notify you with some useful informations about Apex Legends!
+        """)
 
-#@bot.message_handler(content_types=['text'])
-#def handle_command_adminwindow(message):
-#    global msg_count
-#    current_time = datetime.datetime.now().hour
-#    if (current_time < 6):
-#        msg_count = msg_count + 1
-#         if (msg_count == 10):
-        
-#             bot.send_message(chat_id=message.chat.id,
-#                             text="Reply service is unavailable during the midnight",
-#                             parse_mode='HTML')
-#     else:
-    
+@bot.message_handler(commands=['map'])
+def greet(message):
+    bot.send_message(message.chat.id, get_map_informations(get_battle_royale_current_map()))
 
-
-# bot.infinity_polling()
+bot.infinity_polling()
